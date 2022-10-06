c891ca54a9cc098a1732710b2f3fca79aab388a1
SSOsh <tngh147258@naver.com>
2022-10-06 17:38:41 +0900

    Feat : orderList조회 연결완료. orderList css 프로토타입 완료

--- src/Components/Order/orderList.js
+++ src/Components/Order/orderList.js
@@ -4,6 +4,35 @@ import { Link } from "react-router-dom"
 
 import "./orderList.css";
 
+function GetOrderList(order) {
+    console.log("order : ");
+    console.log(order);
+    return (
+        <div className='content'>
+            <div className='order-date'>{order.obj.orderDate}</div>
+            <div className='content-value'>
+                <div className='content-name'>
+                    <div className='name'>{order.obj.name}</div>
+                </div>
+                <div className='content-value-detail'>
+                    <div className='content-thumbnail'>
+                        <img className='thumbnail' src={order.obj.thumbnail} />
+                    </div>
+
+                    <div className='order-value'>
+                        <div className='order-id'>주문번호 {order.obj.orderId}</div>
+                        <br></br>
+                        <div className='price'>결제금액 {order.obj.price}</div>
+                    </div>
+                    <div className='cancel-button'>
+                        <button type='button' className='button'>주문취소</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 function OrderList() {
     const [orders, setOrders] = useState([]);
 
@@ -12,77 +41,30 @@ function OrderList() {
         await axios.get(`http://localhost:8080/orders/${memberId}`
         )
             .then(function (resp) {
-
-                // alert(JSON.parse(resp.data));
-                console.log(resp.data.result.data);
-    
                 setOrders(resp.data.result.data);
-    
             })
             .catch(function (err) {
                 alert(err);
             });
     }
-    
-    const orderList = ({ order }) => {
-
-
-        return (
-            <div>
-                {order.map(order => {
-                    return (
-                        <div>
-                            <div>
-                                {order.name}
-                            </div>
-                            <div>
-                                {order.orderDate}
-                            </div>
-                            <div>
-                                {order.orderId}
-                            </div>
-                            <div>
-                                {order.orderStatus}
-                            </div>
-                            <div>
-                                {order.price}
-                            </div>
-                            <div>
-                                {order.thumbnail}
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
 
     return (
-        <div>
+        <div className='main'>
             <h1>전체 주문 내역</h1>
             <br />
-            <div class="contents">
-                <div class="content">
-                    <div class="order_date">2022.09.22 (13시 46분)</div>
-                    <div class="name">폰타나 오리엔탈 드레싱 외 6건</div>
-                    <div class="thumbnail">"thumbnail"</div>
-                    <div class="order_id">주문번호 12345412345234153412</div>
-                    <div class="price">결제금액 12,300원</div>
-                    <div class="order_status">주문상태 주문완료</div>
-                    <button type="button">주문 취소</button>
-                </div>
-                <div class="content">
-                    <div class="order_date">2022.09.22 (13시 46분)</div>
-                    <div class="name">폰타나 오리엔탈 드레싱 외 6건</div>
-                    <div class="thumbnail">"thumbnail"</div>
-                    <div class="order_id">주문번호 12345412345234153412</div>
-                    <div class="price">결제금액 12,300원</div>
-                    <div class="order_status">주문상태 주문완료</div>
-                    <button type="button">주문 취소</button>
+            <div className="contents">
+                {
+                    orders.map(function (object) {
+                        return (
+                            <GetOrderList obj={object} />
+                        )
+                    })
+                }
+
+                <div className="click-button">
+                    <button onClick={() => getOrderList()}>가져오기</button>
                 </div>
             </div>
-            <button onClick={() => getOrderList()}>가져오기</button>
-            <orderList orders={orders} />
         </div>
 
     )
