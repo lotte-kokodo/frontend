e1bfb2997e1b6fd978e3fcc31f69cd1f15f4076d
ohjiwon-201733026 <ojw970725@gmail.com>
2022-10-14 10:54:11 +0900

    Refactor : Seller 페이지 타이틀 컴포넌트 생성

---
+++ src/components/seller/sellerTitle.js
@@ -0,0 +1,10 @@
+
+import { useParams } from "react-router-dom";
+
+export default function SellerTitle(props) {
+
+
+    return (
+        <h1 style={{marginLeft : "240px", marginTop:"50px"}}>{props.title}</h1>
+    )
+}
