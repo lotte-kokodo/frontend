e487256343127e8e96682c6350bb11c3c0425bf3
hellonayeon <hellonykwon@gmail.com>
2022-10-19 20:47:01 +0900

    Fix: 로컬스토리지에서 memberId 가져와 AuthProvider 에 설정

--- src/context/authProvider.js
+++ src/context/authProvider.js
@@ -6,8 +6,8 @@ function AuthProvider({ children }) {
 
 	/* Http Header 정보 */
 	const [headers, setHeaders] = useState({
-		"Authorization":`Bearer ${localStorage.getItem("accessToken")}`, // 새로고침하면 App Context 사라지기 때문에, 초기 값은 LocalStorage 값으로 세팅
-		"memberId": 3
+		"Authorization":`Bearer ${localStorage.getItem("token")}`, // 새로고침하면 App Context 사라지기 때문에, 초기 값은 LocalStorage 값으로 세팅
+		"memberId": `${localStorage.getItem("memberId")}`
 	});
 
 	// 회원 아이디 정보
