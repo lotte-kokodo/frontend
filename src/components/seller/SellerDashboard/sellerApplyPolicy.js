536aad8558146d16d0884e9b823c022784148d83
SSOsh <tngh147258@naver.com>
2022-11-14 20:24:10 +0900

    Feat : 판매자 정책 관련 대시보드 작성

--- src/components/seller/SellerDashboard/sellerApplyPolicy.js
+++ src/components/seller/SellerDashboard/sellerApplyPolicy.js
@@ -1,12 +1,36 @@
-import React from "react";
 import SellerInfoBox from "./sellerInfoBox";
+import React, {useContext, useState, useEffect} from "react";
+import axios from "axios";
+import {ServerConfigContext} from "../../../context/serverConfigProvider";
+import {AuthContext} from "../../../context/authProvider";
 
 export default function SellerApplyPolicy(){
-    // useEffect(()=>{
-    //
-    // },[]);
+    const { url } = useContext(ServerConfigContext);
+    const {sellerHeaders} = useContext(AuthContext);
 
-    return(
-        <SellerInfoBox></SellerInfoBox>
+    const sellerId = localStorage.getItem("sellerId");
+    let titleName = "이번주 적용된 정책 할인 가격";
+    let unit = "원"
+    const [numberInfo, setNumberInfo] = useState("");
+
+    const getCalculateExpectMount = async () => {
+        await axios.get( url + `/promotion-service/rate-discount/seller/${sellerId}/week`,{headers : sellerHeaders})
+        .then(function (resp) {
+            setNumberInfo(resp.data.result.data);
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    useEffect(()=>{
+        getCalculateExpectMount();
+    },[]);
+
+    return(    
+        <SellerInfoBox titleName={titleName} 
+            numberInfo={numberInfo}
+            unit={unit}
+            >
+        </SellerInfoBox>
     )
 }
