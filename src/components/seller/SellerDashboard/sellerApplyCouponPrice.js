63d000faa2d1654286358e6982e6321239433cc9
SSOsh <tngh147258@naver.com>
2022-11-15 20:14:53 +0900

    Chore : 실행을 위한 중복 import 제거

--- src/components/seller/SellerDashboard/sellerApplyCouponPrice.js
+++ src/components/seller/SellerDashboard/sellerApplyCouponPrice.js
@@ -1,7 +1,5 @@
 import SellerInfoBox from "./sellerInfoBox";
 import {AuthContext} from '../../../context/authProvider';
-
-import axios from 'axios';
 import  { useState, useRef, useEffect } from 'react';
 import { useContext } from "react";
 
@@ -9,9 +7,6 @@ import { ServerConfigContext } from "../../../context/serverConfigProvider";
 
 import React from "react";
 import axios from 'axios';
-import { useState, useRef, useEffect } from 'react';
-import { ServerConfigContext } from "../../../context/serverConfigProvider";
-import { useContext } from "react";
 import {Link} from "react-router-dom"
 
 export default function SellerApplyCouponPolicy(){
