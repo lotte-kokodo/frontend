b524ebf1e493c6fb8dc3edca42d91d17bf051d5f
SSOsh <tngh147258@naver.com>
2022-11-19 22:45:01 +0900

    Chore : ArCoupon 캐치 능력 향상을 위한 z축 수정

--- public/clientApp/app.js
+++ public/clientApp/app.js
@@ -61,8 +61,8 @@ class App {
             if (yleft > yright) {
                 [yleft, yright] = [yright, yleft]
             }
-            var zleft = couponArr[idx].zpos - (couponArr[idx].zpos / 2);
-            var zright = couponArr[idx].zpos + (couponArr[idx].zpos / 2);
+            var zleft = couponArr[idx].zpos - (couponArr[idx].zpos / 2.4);
+            var zright = couponArr[idx].zpos + (couponArr[idx].zpos / 2.4);
             if (zleft > zright) {
                 [zleft, zright] = [zright, zleft]
             }
